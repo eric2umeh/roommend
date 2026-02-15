e41a60d12433c53129f3a90f34e22d76e7321757
Eric Umeh <eric2umeh@yahoo.com>
2026-02-15 17:57:36 +0100

    Add Toast component to Auth Provider

--- app/providers.tsx
+++ app/providers.tsx
@@ -2,7 +2,14 @@
 
 import React from 'react'
 import { AuthProvider } from './lib/auth-context'
+import { ToastProvider } from '@/components/toast-provider'
 
 export function Providers({ children }: { children: React.ReactNode }) {
-  return <AuthProvider>{children}</AuthProvider>
+  return (
+    <AuthProvider>
+      <ToastProvider>
+        {children}
+      </ToastProvider>
+    </AuthProvider>
+  )
 }
